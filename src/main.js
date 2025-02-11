12a782a680fa16ed2a5307e497f7be4451ef8c51
StasSkv <stas000123@gmail.com>
2025-02-12 00:06:59 +0200

    endd

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 
 import { sendRequest } from './js/pixabay-api';
 import { renderImages } from './js/render-functions';
